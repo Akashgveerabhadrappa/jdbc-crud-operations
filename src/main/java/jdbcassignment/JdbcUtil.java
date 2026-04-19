e8166252e3803958cf4cfb4e03685500a7bcf3f3
Akashgveerabhadrappa <akashgveerabhadrappa@gmail.com>
2026-04-19 12:57:11 +0530

    Update JdbcUtil.java

--- src/main/java/jdbcassignment/JdbcUtil.java
+++ src/main/java/jdbcassignment/JdbcUtil.java
@@ -12,7 +12,7 @@ public class JdbcUtil {
     public static Connection getConnection() throws SQLException{
         String url = "jdbc:mysql://localhost:3306/jdbclearning";
         String username = "root";
-        String password = "Akashgv123@";
+        String password = "Your Password";
         return DriverManager.getConnection(url,username,password);
     }
     public static void closeConnection(Statement stmt, Connection conn) throws SQLException{
